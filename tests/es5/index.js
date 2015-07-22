3dd825ea9304a26030b6ae7e556954e4a2121c74
unbornchikken <gabor.mezo@outlook.com>
2015-07-22 09:59:26 +0200

    es5 compatibility

---
+++ tests/es5/index.js
@@ -0,0 +1,3 @@
+"use strict";
+require("./system");
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImluZGV4LmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0FBQUEsTUFBTSxBQUFDLENBQUMsVUFBUyxDQUFDLENBQUM7QUFDbkIiLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoidGVzdHMvZXM2Iiwic291cmNlc0NvbnRlbnQiOlsicmVxdWlyZShcIi4vc3lzdGVtXCIpO1xyXG4iXX0=
